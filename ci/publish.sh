b20c0c1397dc07074f54f7a2d689574031692267
Martin Bories <martin.bories@megatherium.eu>
2023-05-23 15:02:16 +0300

    fix: Build Docker image before publishing

--- ci/publish.sh
+++ ci/publish.sh
@@ -3,6 +3,6 @@
 echo "Publishing to $REPOSITORY_URL"
 REPOSITORY_PASSWORD=`aws ecr get-login-password --region $AWS_REGION`
 docker login --username AWS --password $REPOSITORY_PASSWORD $REPOSITORY_URL
-# docker build -t $IMAGE_NAME .
-# docker tag $IMAGE_NAME:latest $REPOSITORY_URL:latest
+docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME:latest $REPOSITORY_URL:latest
 docker push $REPOSITORY_URL:latest
