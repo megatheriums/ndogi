fcc19f8c9190ad8a16227df96388f41f32638243
Martin Bories <martin.bories@megatherium.eu>
2023-05-23 12:18:44 +0300

    ci: Correctly use exit-codes

--- ci/test.sh
+++ ci/test.sh
@@ -10,17 +10,31 @@ echo "Running $objective tests..."
 # Terraform formatting
 if [ "$objective" = "all" ] || [ "$objective" = "terraform-lint" ]; then
 	cd infrastructure
-	env $(cat .env | xargs) terragrunt fmt -check || (echo "Failed terraform linting" && exit 1)
-    echo "Terraform linting succeeded"
+	env $(cat .env | xargs) terragrunt fmt -check
+    exit_code=$?
+    if [ exit_code != 0 ]; then
+        echo "Failed terraform linting"
+        exit $exit_code
+    fi
 	cd ..
 fi
 
 # Audit NPM
 if [ "$objective" = "all" ] || [ "$objective" = "node-audit" ]; then
-	npm audit || (echo "Failed NPM Audit" && exit 1)
+	npm audit
+    exit_code=$?
+    if [ exit_code != 0 ]; then
+        echo "Failed NPM Audit"
+        exit $exit_code
+    fi
 fi
 
 # Node.JS Format
 if [ "$objective" = "all" ] || [ "$objective" = "node-lint" ]; then
-	./node_modules/.bin/eslint src || (echo "Failed Node.JS Linting" && exit 1)
+	./node_modules/.bin/eslint src
+    exit_code=$?
+    if [ $exit_code != 0 ]; then
+        echo "Failed Node.JS Linting"
+        exit $exit_code
+    fi
 fi
